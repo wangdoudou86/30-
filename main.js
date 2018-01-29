532b924908d3377fc2270acf6a9e569b75e20d7b
krystal <892184375@qq.com>
2018-01-29 23:45:59 +0800

    优化代码

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
-$('.images>img:nth-child(1)').addClass('current')
-$('.images>img:nth-child(2)').addClass('enter')
-$('.images>img:nth-child(3)').addClass('enter')
-$('.images>img:nth-child(4)').addClass('enter')
-let n = 1
+let n 
+初始化()
 
 setInterval(()=>{
-    $(`.images>img:nth-child(${x(n)})`).removeClass('current').addClass('leave').one(
-        'transitionend',(x)=>{$(x.currentTarget).removeClass('leave').addClass('enter')}
-    )
-    $(`.images>img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+    makeLeave(getImage(n))
+    .one('transitionend',(x)=>{makeEnter($(x.currentTarget))
+})
+makeCurrent(getImage(n+1))
     n += 1
-},2000)
+},3000)
+
+
+
+
+
+
 function x(n){
     if(n>4){
         n = n%4
         if(n===0)
         {n=4}
     }
-    return n
+    return n // 1 2 3 4
+}
+function 初始化(){
+    n = 1 //踩的坑：这里不要再写let了
+    $(`.images>img:nth-child(${n}`).addClass('current').siblings().addClass('enter')
+}
+function makeLeave($node){
+    $node.removeClass('current enter').addClass('leave')
+    return $node //如果不写这句，makeLeave()的返回值就是undefined，这样就无法调用one()方法，所以再把$node给传出去
+}
+function makeEnter($node){
+    $node.removeClass('current leave').addClass('enter')
+    return $node
+}
+function makeCurrent($node){
+    $node.removeClass('enter leave').addClass('current')
+}
+function getImage(n){
+    return $(`.images>img:nth-child(${x(n)})`)
 }
